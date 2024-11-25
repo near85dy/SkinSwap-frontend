3dbf10ee337a57e944c3678276af23eddb0e40fe
Arguts Pužuls <aarguts12@gmail.com>
2024-11-25 08:30:49 +0200

    my profile, fixed

--- skinswap/src/components/Header.jsx
+++ skinswap/src/components/Header.jsx
@@ -1,49 +1,4 @@
-function Header() {
-  const API_URL = "http://localhost:8080/api";
-  const handleLoginButton = async () => {
-      try {
-          const response = await fetch(API_URL + "/auth/steam/login");
-          const data = await response.json();
-
-          if (data.steamLoginUrl) {
-              window.location.href = data.steamLoginUrl;
-          }
-      } catch (error) {
-          console.error("Error fetching Steam login URL:", error);
-      }
-  };
-
-  return (
-      <header>
-          <div className="header-logo">
-              {/* Logo Container */}
-              <div className="logo-container">
-                  {/* Triangle Background */}
-                  <div className="logo-shape">
-                      <span className="lightning-icon">⚡</span>
-                  </div>
-                  {/* Logo Text */}
-                  <div className="logo-text">
-                      <h1 className="logo-title">CSTRADE</h1>
-                    
-                  </div>
-              </div>
-          </div>
-          <nav className="header-links" id="header-links">
-              <form className="login-form">
-                  <button onClick={handleLoginButton} className="login-button">Вход</button>
-              </form>
-              <ul className="menu">
-                  <li><a href="profile">Profile</a></li>
-              </ul>
-          </nav>
-      </header>
-  );
-}
-
-export default Header;
-
-  /*function Header() 
+function Header() 
 {
   const API_URL = "http://localhost:8080/api";
   const handleLoginButton = async () =>
@@ -86,4 +41,4 @@ export default Header;
   }
   
   export default Header;
-   */
+   
